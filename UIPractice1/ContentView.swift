eae080186f8c43c7eb07bc2011a65c5963a5f873
tanvir <tanvirgeek@gmail.com>
2020-12-02 11:29:30 +0600

    Initial Commit

---
+++ UIPractice1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UIPractice1
+//
+//  Created by MD Tanvir Alam on 2/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UIPractice1/UIPractice1App.swift
@@ -0,0 +1,17 @@
+//
+//  UIPractice1App.swift
+//  UIPractice1
+//
+//  Created by MD Tanvir Alam on 2/12/20.
+//
+
+import SwiftUI
+
+@main
+struct UIPractice1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
